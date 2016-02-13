33412e662e4ad8074ac7878a2b0c624d0cbe6c1b
Tomek <tomek.stasiak@gmail.com>
2016-02-13 12:34:07 +0100

    added views injection

---
+++ slidingPanel/src/main/java/com/tomek/slidingpanel/panel/activity/PanelActivity.java
@@ -0,0 +1,34 @@
+package com.tomek.slidingpanel.panel.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.tomek.slidingpanel.R;
+import com.tomek.slidingpanel.utils.activity.BaseActivity;
+
+import butterknife.Bind;
+
+public class PanelActivity extends BaseActivity {
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
